7ec8af6d96c1be1c0a809400699ec85a4896fa96
nicevibesplus <andirad99@gmail.com>
2024-06-07 19:17:34 +0200

    add uploadBaseURL as config param

--- config.js
+++ config.js
@@ -1,25 +1,27 @@
 module.exports = {
-  httpPort: 2344,
-  baseURL: '/protokolle', // url prefix, eg '/editor'
+    httpPort: 2344,
+    baseURL: '/protokolle', // url prefix, eg '/editor',
+    uploadBaseURL: 'x/', // base url where to inspect uploaded pdf documents, eg. https://server.de/uploads/protokolle/
 
-  // paths where the documents will be loaded & saved
-  // subdirectories are not accessible
-  directories: {
-    documents: __dirname + '/data/documents/',
-    templates: __dirname + '/data/templates/',
-  },
 
-  // only files with these extensions will be shown in the filelist
-  listExtensions: ['.md', '.markdown', '.mmd'],
+    // paths where the documents will be loaded & saved
+    // subdirectories are not accessible
+    directories: {
+        documents: __dirname + '/data/documents/',
+        templates: __dirname + '/data/templates/',
+    },
 
-  // formats available for export using pandoc, see http://pandoc.org/README.html
-  exportFormats: {
-    'PDF': { extension: '.pdf', options: '-t latex -V geometry:margin=2.4cm -N'},
-    'LaTeX': { extension: '.tex', options: '-t latex -V geometry:margin=2.4cm -s'},
-    'HTML': { extension: '.html', options: '-t html5 -s'},
-    'reveal.js-Slides': { extension: '-revealjs.html', options: '-t revealjs -s'},
-    'MediaWiki': { extension: '.mediawiki', options: '-t mediawiki -s'},
-    'DokuWiki':  { extension: '.dokuwiki', options: '-t dokuwiki -s'},
-    'DOCX':  { extension: '.docx', options: ''},
-  }
+    // only files with these extensions will be shown in the filelist
+    listExtensions: ['.md', '.markdown', '.mmd'],
+
+    // formats available for export using pandoc, see http://pandoc.org/README.html
+    exportFormats: {
+        'PDF': { extension: '.pdf', options: '-t latex -V geometry:margin=2.4cm -N' },
+        'LaTeX': { extension: '.tex', options: '-t latex -V geometry:margin=2.4cm -s' },
+        'HTML': { extension: '.html', options: '-t html5 -s' },
+        'reveal.js-Slides': { extension: '-revealjs.html', options: '-t revealjs -s' },
+        'MediaWiki': { extension: '.mediawiki', options: '-t mediawiki -s' },
+        'DokuWiki': { extension: '.dokuwiki', options: '-t dokuwiki -s' },
+        'DOCX': { extension: '.docx', options: '' },
+    }
 };
